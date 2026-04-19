93bcfe643c996246b562ed2a4c755d04e2e32707
konstantinlokotunin <konstantin.lokotunin@sciencespo.fr>
2026-04-20 00:16:33 +0200

    Fix merge conflicts in extract.py and visualization.py

--- src/visualization.py
+++ src/visualization.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib.dates as mdates
@@ -78,19 +76,3 @@ def visualize_data(df):
     plt.tight_layout()
     plt.savefig("outputs/fuel_prices.png", dpi=300)
     plt.close(fig)
-=======
-fig, ax = plt.subplots(figsize=(10, 5))
-
-ax.plot(df["date"], df["EU_Price_Super95"], label="EU Petrol")
-ax.plot(df["date"], df["AT_Price_Super95"], label="Austria Petrol")
-ax.plot(df["date"], df["DE_Price_Super95"], label="Germany Petrol")
-
-ax.set_title("Petrol Prices Comparison (EU vs AT vs DE)")
-ax.legend()
-
-ax.grid(True, axis="y", alpha=0.2)
-sns.despine(ax=ax)
-
-plt.xticks(rotation=30)
-plt.tight_layout()
->>>>>>> c1cb5c3261f5dd09bfb191177f6ac01d56feb379
