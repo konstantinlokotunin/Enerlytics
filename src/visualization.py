3c471d3e9169b874f4c722935a3a81d219af091d
k-lokotunin <konstantin.lokotunin@sciencespo.fr>
2026-04-29 18:31:31 +0200

    Refactor visualization function to use subplots

--- src/visualization.py
+++ src/visualization.py
@@ -5,34 +5,34 @@ import pandas as pd
 
 sns.set_theme(style="white", context="talk")
 
-def style_ax(ax):
+def style_ax(axs):
     # Background
-    ax.set_facecolor("#ffffff")  # bright snow
+    axs.set_facecolor("#ffffff")  # bright snow
 
     # Grid (subtle, vertical only)
-    ax.grid(True, axis="y", color="#9ca3af", linestyle="--", alpha=0.4)
+    axs.grid(True, axis="y", color="#9ca3af", linestyle="--", alpha=0.4)
 
     # Customize spines
-    sns.despine(ax=ax, left=False, bottom=False)
+    sns.despine(ax=axs, left=False, bottom=False)
 
     for spine in ["left", "bottom", "right", "top"]:
-        ax.spines[spine].set_linewidth(1.25)
-        ax.spines[spine].set_color("#9ca3af")
+        axs.spines[spine].set_linewidth(1.25)
+        axs.spines[spine].set_color("#9ca3af")
 
     # Tick styling
-    ax.tick_params(axis='x', labelsize=11, rotation=30)
-    ax.tick_params(axis='y', labelsize=12)
+    axs.tick_params(axis='x', labelsize=11, rotation=30)
+    axs.tick_params(axis='y', labelsize=12)
 
     # Labels
-    ax.yaxis.label.set_color("#242424")
-    ax.xaxis.label.set_color("#242424")
-    ax.set_ylabel("€/L", fontsize=14)
+    axs.yaxis.label.set_color("#242424")
+    axs.xaxis.label.set_color("#242424")
+    axs.set_ylabel("€/L", fontsize=14)
 
     # Title
-    ax.title.set_color("#242424")
+    axs.title.set_color("#242424")
     
     # Grid
-    ax.legend(
+    axs.legend(
         frameon=False,
         ncol=2,
         fontsize=10,
@@ -41,7 +41,7 @@ def style_ax(ax):
 
 def visualize_data(df, window=4):
 
-    fig, ax = plt.subplots(figsize=(16, 7))
+    fig, axs = plt.subplots(4, 1, figsize=(16, 14), sharex=True)
 
     # --- COLOR SYSTEM (by country) ---
     COLORS = {
@@ -52,10 +52,11 @@ def visualize_data(df, window=4):
 
     # --- PLOT (grouped logic) ---
     for col in df.columns:
-        if col == "Date":
+        if col == "Date" or "EU" not in col:
             continue
 
-        country = col.split("_")[0]   # EU, AT
+        if "EU" in col:
+            country = col.split("_")[0]   # EU, AT
 
         if "Gasoline" in col:
             linestyle = "-"
@@ -64,10 +65,19 @@ def visualize_data(df, window=4):
             linestyle = "--"
             alpha = 0.9
 
-        ax.plot(
+        axs[0].plot(
+            df["Date"],
+            df[col],
+            label=col,
+            color=COLORS[country],
+            linestyle=linestyle,
+            alpha=0.2
+        )
+
+        axs[0].plot(
             df["Date"],
             df[col].rolling(window).mean(),
-            label=col.replace("_", " "),
+            label=f"{col} '- Rolling Average'",
             color=COLORS[country],
             linewidth=2.2,
             linestyle=linestyle,
@@ -75,32 +85,36 @@ def visualize_data(df, window=4):
         )
 
     # --- TITLE ---
-    ax.set_title(
-        "Fuel Prices in Europe (EU vs Austria)",
+    axs[0].set_title(
+        "EU Fuel Prices (Raw vs Smoothed)",
         fontsize=18,
         pad=15,
         weight="bold"
     )
 
     # --- Y RANGE ---
-    numeric_df = df.drop(columns=["Date"])
+    for col in df.columns:
+        
+        if col == "Date" or "EU" not in col:    
+
+            numeric_df = df.drop(columns=col)
 
     y_min = numeric_df.min().min()
     y_max = numeric_df.max().max()
 
-    ax.set_ylim(y_min - 0.05, y_max + 0.05)
+    axs[0].set_ylim(y_min - 0.05, y_max + 0.05)
 
     # --- X AXIS ---
-    ax.xaxis.set_major_locator(mdates.MonthLocator(interval=3))
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+    axs.xaxis.set_major_locator(mdates.MonthLocator(interval=3))
+    axs.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
 
-    ax.axvspan(pd.Timestamp("2022-02-24"), pd.Timestamp("2023-01-01"),
+    axs.axvspan(pd.Timestamp("2022-02-24"), pd.Timestamp("2023-01-01"),
         color="#9ca3af", alpha=0.1, label="Ukraine war")
     
-    ax.axvspan(pd.Timestamp("2026-02-28"), df["Date"].max(),
+    axs.axvspan(pd.Timestamp("2026-02-28"), df["Date"].max(),
         color="#9ca3af", alpha=0.1, label="2026 Iran war")
 
-    style_ax(ax)
+    style_ax(axs)
 
     plt.tight_layout()
 
