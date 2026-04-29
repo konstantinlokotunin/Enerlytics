ed39cf6473c3ff3536e0c7e0d7b7d48771144ad2
k-lokotunin <konstantin.lokotunin@sciencespo.fr>
2026-04-29 20:21:21 +0200

    Clean up main function by removing blank line
    Remove unnecessary blank line in main function

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from src.extract import extract_data
 from src.transform import transform_data
 from src.visualization import visualize_data
 
-
 def main():
     df = extract_data()
     df = transform_data(df)
