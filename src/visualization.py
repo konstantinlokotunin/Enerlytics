9d56374d9edb83c7d9a8ceb634f332e237abd3ca
konstantinlokotunin <konstantin.lokotunin@sciencespo.fr>
2026-04-29 20:39:33 +0200

    Remove dropna() from rolling calculations in visualize_data function

--- src/visualization.py
+++ src/visualization.py
@@ -77,7 +77,7 @@ def visualize_data(df, window=4):
 
     ax1.plot(
         df["Date"],
-        df[relevant_columns].rolling(window).mean().dropna(),
+        df[relevant_columns].rolling(window).mean(),
         label=[f"{col} - Rolling Average" for col in relevant_columns],
         color=COLORS["EU"],
         linewidth=2.2,
@@ -130,7 +130,7 @@ def visualize_data(df, window=4):
 
     ax2.plot(
         df["Date"],
-        df[relevant_columns].rolling(window).std().dropna(),
+        df[relevant_columns].rolling(window).std(),
         label=f"{relevant_columns} Volatility",
         color=COLORS["EU"],
         linewidth=2.2,
