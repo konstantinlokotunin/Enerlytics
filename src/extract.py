9048af44411ede98a33caf1af0531148c4d76642
konstantinlokotunin <konstantin.lokotunin@sciencespo.fr>
2026-04-30 22:37:38 +0200

    Update data extraction in extract_data function to use a remote URL for the Excel file

--- src/extract.py
+++ src/extract.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
 def extract_data():
-    df = pd.read_excel(
-        "data/Weekly_Oil_Bulletin_Prices_History_maticni_4web.xlsx",
-        header=[0,1,2]
-    )
+
+    url = "https://energy.ec.europa.eu/document/download/906e60ca-8b6a-44e7-8589-652854d2fd3f_en?" \
+    "filename=Weekly_Oil_Bulletin_Prices_History_maticni_4web.xlsx"
+
+    df = pd.read_excel(url, header=[0,1,2])
+
     return df
